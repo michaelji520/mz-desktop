1e60b90d03d18389b5d1e157abe6107e95f84ac6
Michael Zhang <michaelji520@gmail.com>
2021-10-18 23:59:06 +0800

    feat: init desktop icons

---
+++ src/apps/apps.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: 'Michael',
+    icon: require('./assets/computer.png').default
+  },
+  {
+    name: 'Recycle Bin',
+    icon: require('./assets/recycle.png').default
+  },
+  {
+    name: 'Transmission',
+    icon: require('./assets/transmission.png').default
+  },
+];
--- src/components/mz-application/mz-application.ts
+++ src/components/mz-application/mz-application.ts
@@ -1,21 +1,75 @@
 class MZApplication extends HTMLElement {
   constructor() {
     super();
-    const shadowRoot = this.attachShadow({mode: 'closed'});
+    const shadowRoot = this.attachShadow({mode: 'open'});
 
     shadowRoot.innerHTML = `
     <style>
     :host {
       display: flex;
-      padding: 16px;
-      width: 56px;
-      height: 66px;
-      background: #FFF;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 12px 12px 0 12px;
+      transition: all .2s ease-in-out;
+    }
+    :host(:hover) {
+      background-color: hsla(0,0%,100%,.25)
+    }
+    img {
+      width: 36px;
+      height: 36px;
+    }
+    span {
+      height: 34px;
+      line-height: 17px;
+      max-width: 54px;
+      font-size: .8em;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      text-align: center;
+      color: #fafafa;
+      margin: 4px 0;
+      text-shadow: 0 0 4px rgb(0 0 0 / 60%);
     }
     </style>
-    <slot></slot>
+    <img src="" />
+    <span></span>
     `;
     
+
+  }
+
+  /**
+   * @description 当 custom element首次被插入文档DOM时，被调用。
+   */
+  connectedCallback() {
+    const icon = this.getAttribute('icon');
+    const name = this.getAttribute('name');
+    console.log(icon, name);
+    this.shadowRoot.querySelector('img').src = icon;
+    this.shadowRoot.querySelector('span').textContent= name;
+
+  }
+
+  /**
+   * @description 当 custom element从文档DOM中删除时，被调用。
+   */
+  disconnectedCallback() {
+    
+  }
+
+  /**
+   * @description 当 custom element被移动到新的文档时，被调用。
+   */
+  adoptedCallback() {
+
+  }
+
+  /**
+   * @description 当 custom element增加、删除、修改自身属性时，被调用。
+   */
+  attributeChangedCallback() {
 
   }
 }
