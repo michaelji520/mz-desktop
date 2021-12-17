35f7ca842030039c83a36b2b9c72d77692b4c976
Michael Zhang <michaelji520@gmail.com>
2021-12-17 06:02:22 +0000

    feat: add animation & init manifest plugin

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const WebpackPwaManifest  = require('webpack-pwa-manifest');
+
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -72,5 +74,27 @@ module.exports = {
       chunks: ['main']
     }),
     isProd && new CleanWebpackPlugin(),
+    isProd && new WebpackPwaManifest({
+      name: 'MZ Desktop Toolkit',
+      short_name: 'MZDesktop',
+      description: 'My awesome Progressive Web App!',
+      background_color: '#CCCCCC',
+      crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+      icons: [
+        {
+          src: path.resolve(__dirname, '../src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+        },
+        {
+          src: path.resolve('src/assets/large-icon.png'),
+          size: '1024x1024' // you can also use the specifications pattern
+        },
+        {
+          src: path.resolve('src/assets/maskable-icon.png'),
+          size: '1024x1024',
+          purpose: 'maskable'
+        }
+      ]
+    })
   ].filter(i => !!i)
 };
